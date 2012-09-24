b83aab0f7e279c83ff41fd0076e5822439d120cd
Jeff Bean <jeffmbean@gmail.com>
2012-09-23 20:20:40 -0600

    Initial Commit

---
+++ Apathy/Backup/Apathy/DAL/BudgetContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace Apathy.Models
+{
+    public class BudgetContext : DbContext
+    {
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<Envelope> Envelopes { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
