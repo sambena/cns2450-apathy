673c346f726bf7c4d66acaf020c8b2ffec96acf1
Branden McDonald <mcd.branden@gmail.com>
2012-09-27 17:17:26 -0600

    Added Transaction and Envelope Controllers and Views

---
+++ Apathy/Apathy/Controllers/EnvelopeController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Apathy.Models;
+
+namespace Apathy.Controllers
+{ 
+    public class EnvelopeController : Controller
+    {
+        private BudgetContext db = new BudgetContext();
+
+        //
+        // GET: /Envelope/
+
+        public ViewResult Index()
+        {
+            var envelopes = db.Envelopes.Include(e => e.Budget);
+            return View(envelopes.ToList());
+        }
+
+        //
+        // GET: /Envelope/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Envelope envelope = db.Envelopes.Find(id);
+            return View(envelope);
+        }
+
+        //
+        // GET: /Envelope/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.BudgetID = new SelectList(db.Budgets, "BudgetID", "Owner");
+            return View();
+        } 
+
+        //
+        // POST: /Envelope/Create
+
+        [HttpPost]
+        public ActionResult Create(Envelope envelope)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Envelopes.Add(envelope);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.BudgetID = new SelectList(db.Budgets, "BudgetID", "Owner", envelope.BudgetID);
+            return View(envelope);
+        }
+        
+        //
+        // GET: /Envelope/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Envelope envelope = db.Envelopes.Find(id);
+            ViewBag.BudgetID = new SelectList(db.Budgets, "BudgetID", "Owner", envelope.BudgetID);
+            return View(envelope);
+        }
+
+        //
+        // POST: /Envelope/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Envelope envelope)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(envelope).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BudgetID = new SelectList(db.Budgets, "BudgetID", "Owner", envelope.BudgetID);
+            return View(envelope);
+        }
+
+        //
+        // GET: /Envelope/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Envelope envelope = db.Envelopes.Find(id);
+            return View(envelope);
+        }
+
+        //
+        // POST: /Envelope/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Envelope envelope = db.Envelopes.Find(id);
+            db.Envelopes.Remove(envelope);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Apathy/Apathy/Controllers/TransactionsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Apathy.Models;
+
+namespace Apathy.Controllers
+{ 
+    public class TransactionsController : Controller
+    {
+        private BudgetContext db = new BudgetContext();
+
+        //
+        // GET: /Transactions/
+
+        public ViewResult Index()
+        {
+            var transactions = db.Transactions.Include(t => t.Envelope);
+            return View(transactions.ToList());
+        }
+
+        //
+        // GET: /Transactions/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            return View(transaction);
+        }
+
+        //
+        // GET: /Transactions/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.EnvelopeID = new SelectList(db.Envelopes, "EnvelopeID", "Title");
+            return View();
+        } 
+
+        //
+        // POST: /Transactions/Create
+
+        [HttpPost]
+        public ActionResult Create(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.EnvelopeID = new SelectList(db.Envelopes, "EnvelopeID", "Title", transaction.EnvelopeID);
+            return View(transaction);
+        }
+        
+        //
+        // GET: /Transactions/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            ViewBag.EnvelopeID = new SelectList(db.Envelopes, "EnvelopeID", "Title", transaction.EnvelopeID);
+            return View(transaction);
+        }
+
+        //
+        // POST: /Transactions/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transaction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EnvelopeID = new SelectList(db.Envelopes, "EnvelopeID", "Title", transaction.EnvelopeID);
+            return View(transaction);
+        }
+
+        //
+        // GET: /Transactions/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            return View(transaction);
+        }
+
+        //
+        // POST: /Transactions/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Transaction transaction = db.Transactions.Find(id);
+            db.Transactions.Remove(transaction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
