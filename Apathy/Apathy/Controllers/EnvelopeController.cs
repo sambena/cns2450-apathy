65328f410c80a510deed02416d0864efbb5a29da
Sam Bryan <sambena@gmail.com>
2012-09-27 20:45:25 -0600

    Create Envelopes page

---
+++ Apathy/Apathy/Controllers/EnvelopeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Apathy.Models;
+
+namespace Apathy.Controllers
+{
+    public class EnvelopeController : Controller
+    {
+        //
+        // GET: /Envelope/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
