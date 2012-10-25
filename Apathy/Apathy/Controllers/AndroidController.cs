6e09264748f3fc7b12ef98cead638e0195d5b555
Matthew Tolman <matt@mptolman.net>
2012-10-25 15:25:05 -0600

    Added controller for Android operations

---
+++ Apathy/Apathy/Controllers/AndroidController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Apathy.DAL;
+
+namespace Apathy.Controllers
+{
+    public class AndroidController : Controller
+    {
+        //
+        // GET: /Android/GetEnvelopes/username
+        public ActionResult GetEnvelopes(string id)
+        {
+            var envelopes = from e in Services.EnvelopeService.GetEnvelopes(id)
+                            select new { e.EnvelopeID, e.Title, e.StartingBalance, e.CurrentBalance };
+
+            return Json(envelopes, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
