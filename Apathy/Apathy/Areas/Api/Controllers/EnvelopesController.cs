0643a64c0fc014a4abe6682a115ac2093722820c
Matthew Tolman <matt@mptolman.net>
2012-11-14 15:26:18 -0700

    Mobile API - Envelopes support
    Added support for retrieving, creating, editing, and deleting envelopes
    via mobile API (Json)

---
+++ Apathy/Apathy/Areas/Api/Controllers/EnvelopesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Apathy.Models;
+using Apathy.DAL;
+
+namespace Apathy.Areas.Api.Controllers
+{
+    public class EnvelopesController : Controller
+    {
+        public JsonResult Index(string username)
+        {
+            var envelopes = from e in Services.EnvelopeService.GetEnvelopes(username)
+                            select new { e.EnvelopeID, e.Title, e.CurrentBalance, e.StartingBalance };
+
+            return Json(new { success = true, data = envelopes }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Get(string username, int id)
+        {
+            Envelope envelope = Services.EnvelopeService.GetEnvelope(id, username);
+            if (envelope == null)
+                return Json(new { success = false });
+
+            return Json(new { success = true, data = new { envelope.EnvelopeID, envelope.CurrentBalance, envelope.StartingBalance } }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Create(string username, Envelope envelope)
+        {
+            Services.EnvelopeService.InsertEnvelope(envelope, username);
+
+            return Json(new { success = true, data = new { envelope.EnvelopeID, envelope.CurrentBalance, envelope.StartingBalance } });
+        }
+
+        [HttpPost]
+        public JsonResult Edit(Envelope envelope)
+        {
+            Services.EnvelopeService.UpdateEnvelope(envelope);
+
+            return Json(new { success = true, data = new { envelope.EnvelopeID, envelope.CurrentBalance, envelope.StartingBalance } });
+        }
+
+        [HttpPost]
+        public JsonResult Delete(string username, int id)
+        {
+            Services.EnvelopeService.DeleteEnvelope(id, username);
+
+            return Json(new { success = true });
+        }
+    }
+}
